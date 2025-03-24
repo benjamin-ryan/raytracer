7eef5ac26a0316bd5846dc87387f1bc1216a5dbc
Benjamin <benjamin.ryan@comcast.net>
2025-03-24 19:04:06 -0400

    .gitignore added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#define WIDTH 256
+#define HEIGHT 256
+
+int main()
+{
+    std::cout << "P3\n" << WIDTH << ' ' << HEIGHT << "\n255\n";
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            float r = double(j) / (WIDTH - 1);
+            float g = double(i) / (HEIGHT - 1);
+            float b = 0.0;
+
+            int i_r = int(255.9999 * r);
+            int i_g = int(255.9999 * g);
+            int i_b = int(255.9999 * b);
+
+            std::cout << i_r << ' ' << i_g << ' ' << i_b << "\n";
+        }
+    }
 }
